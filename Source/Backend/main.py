c328c5109e060ae621d96a1b1be5f37e47d1db6d
LeoSiegmund <leonhard.siegmund@online.de>
2021-03-25 10:19:47 +0100

    User Table Added

--- Source/Backend/main.py
+++ Source/Backend/main.py
@@ -6,6 +6,27 @@ import os
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+
+
+# User Class
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+
 # Run App
 if __name__ == '__main__':
     app.run(debug=True)
