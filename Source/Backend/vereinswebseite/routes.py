3a652c09b895f5016ad274fd8d2dac77f22382c0
LeoSiegmund <leonhard.siegmund@online.de>
2021-03-25 11:56:23 +0100

    Create User implemented

--- Source/Backend/vereinswebseite/routes.py
+++ Source/Backend/vereinswebseite/routes.py
@@ -1,6 +1,24 @@
-from vereinswebseite import app
+from vereinswebseite import app, db
+from vereinswebseite.models import User
+from flask import request
 
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+
+# Create User Data
+@app.route('/user', methods=['POST'])
+def add_user():
+    name = request.json['name']
+    email = request.json['email']
+    password = request.json['password']
+
+    new_user = User(name, email, password)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return 201
+    except Exception:
+        return Exception.args
