b2c835f28a5cba8d61b52b2abe97e1947626e537
CodingMarco <codingmarco@gmail.com>
2021-03-25 11:48:03 +0100

    Moved User class to models.py

--- Source/Backend/vereinswebseite/models.py
+++ Source/Backend/vereinswebseite/models.py
@@ -1,1 +1,13 @@
- 
+from vereinswebseite import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
