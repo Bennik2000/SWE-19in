04079bf8623eba8e6620d352a71a834bb16e0e10
JannikWell <jawell77@gmail.com>
2021-03-26 11:24:37 +0100

    Included an extension for converting ts into js
    Extension = "Compile Hero"
    -->Converts .ts files into .js files and stores them into the public/src foulder

--- Source/Frontend/server.js
+++ Source/Frontend/server.js
@@ -2,10 +2,10 @@ const path = require("path")
 const express = require("express")
 const app = express()
 
-app.set('views', path.join(__dirname, '/../public/views'));
+app.set('views', path.join(__dirname, '/public/views'));
 app.set("view engine", "ejs")
 
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
     res.render("index")
