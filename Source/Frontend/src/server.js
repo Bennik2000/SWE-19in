664d681b98c80d8e3897a1c8e49acbd796ce2cd3
JannikWell <jawell77@gmail.com>
2021-03-26 09:58:52 +0100

    Small fix for showing own css
    After changing the directory structure, the self created css wasnt displayed

--- Source/Frontend/src/server.js
+++ Source/Frontend/src/server.js
@@ -5,7 +5,7 @@ const app = express()
 app.set('views', path.join(__dirname, '/../public/views'));
 app.set("view engine", "ejs")
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../public'));
 
 app.get("/", (req, res) => {
     res.render("index")
