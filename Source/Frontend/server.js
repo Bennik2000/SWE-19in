7c2b08aa64bf7f24d55d2694e9a21672cce237af
JannikWell <jawell77@gmail.com>
2021-03-26 09:22:12 +0100

    Own css files can be used along with bootsrap

--- Source/Frontend/server.js
+++ Source/Frontend/server.js
@@ -1,9 +1,11 @@
+const path = require("path")
 const express = require("express")
 const app = express()
 
+app.set('views', path.join(__dirname, '/public/views'));
 app.set("view engine", "ejs")
 
-//app.use(express.static(__dirname + '/Frontend'));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
     res.render("index")
