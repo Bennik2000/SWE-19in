a707d8579c3b9a84f3947d7a95bfe306f50e4d92
JannikWell <jawell77@gmail.com>
2021-03-26 09:49:17 +0100

    Changed the directory structure

--- Source/Frontend/src/server.js
+++ Source/Frontend/src/server.js
@@ -2,7 +2,7 @@ const path = require("path")
 const express = require("express")
 const app = express()
 
-app.set('views', path.join(__dirname, '/public/views'));
+app.set('views', path.join(__dirname, '/../public/views'));
 app.set("view engine", "ejs")
 
 app.use(express.static(__dirname + '/public'));
