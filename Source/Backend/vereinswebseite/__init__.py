9f7c656a69d0a885ce8171e9dc5050dbf49a0d0d
CodingMarco <codingmarco@gmail.com>
2021-03-31 23:17:26 +0200

    Added login manager and modified user model

--- Source/Backend/vereinswebseite/__init__.py
+++ Source/Backend/vereinswebseite/__init__.py
@@ -1,15 +1,25 @@
+import datetime
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_login import LoginManager
+
 
 app = Flask("VereinSWEbseite")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "M2JjYjU2NDZmYzUJhMIgIC0K"
+app.config["REMEMBER_COOKIE_DURATION"] = datetime.timedelta(weeks=12)
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+login_manager = LoginManager(app)
 
 # Ignore PEP8 this one time to have the routes in a separate file,
 # while avoiding circular imports
 from vereinswebseite import routes  # noqa: E402
 from vereinswebseite import routes_users  # noqa: E402
 from vereinswebseite import routes_articles  # noqa: E402
+
+db.create_all()
--- Source/Backend/vereinswebseite/models.py
+++ Source/Backend/vereinswebseite/models.py
@@ -1,4 +1,6 @@
 from vereinswebseite import db, ma
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Article(db.Model):
@@ -15,16 +17,17 @@ class Article(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
 
     articles = db.relationship("Article")
 
-    def __init__(self, name, email, password):
-        self.name = name
-        self.email = email
-        self.password = password
+    def set_password(self, password):
+        self.password = generate_password_hash(password, method='sha256')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 class UserSchema(ma.Schema):
