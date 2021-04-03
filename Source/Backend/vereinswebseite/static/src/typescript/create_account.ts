cc7a6492201bb9f86b5285bec1be201a9f50d859
JannikWell <jawell77@gmail.com>
2021-04-03 15:41:24 +0200

    Create account with user input
    Tokens are ignored by now

--- Source/Backend/vereinswebseite/static/src/typescript/create_account.ts
+++ Source/Backend/vereinswebseite/static/src/typescript/create_account.ts
@@ -1,22 +1,42 @@
 var text = '{"email": "jonas1.hille@email.com", "name": "Jonas1 Hille 2","password": "123456"}';
 function createAccount() {
-    var newEmail = document.getElementById("email");
-    var firstname = document.getElementById("firstname");
-    var secondname = document.getElementById("secondname");
-    var newPassword = document.getElementById("password");
-    var newPassword2 = document.getElementById("password2");
-    var newToken = document.getElementById("token");
-    var field = "my_field_name";
-    var obj = {};
-    obj["email"] = newEmail;
-    obj["name"] = new String(firstname + " " + secondname);
-    obj["password"] = newPassword;
-    obj["token"] = newToken;
-    var myJSON = JSON.stringify(obj);
-    console.log(myJSON);
-    /*var xhttp = new XMLHttpRequest();
-    var url = "/user";
-    xhttp.open("POST", url, true);
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(myJSON);*/
+
+    var newEmail = document.getElementById("email") as HTMLInputElement;
+    var firstName = document.getElementById("firstname") as HTMLInputElement;
+    var secondName = document.getElementById("secondname") as HTMLInputElement;
+    var newPassword = document.getElementById("password")as HTMLInputElement;
+    var newPassword2 = document.getElementById("password2")as HTMLInputElement;
+    var newToken = document.getElementById("token")as HTMLInputElement;
+
+    if(newPassword.value != newPassword2.value) {
+     alert("Passwörter stimmen nicht überein. \n Bitte überprüfen!"); 
+     newEmail.value = newEmail.value;
+     return;  
+    }
+
+    if (newEmail.value != "" && firstName.value != "" && secondName.value != "" &&
+        newPassword.value != "" && newPassword2.value != "" && newToken.value != "") {
+        var obj = {};
+        obj["email"] = newEmail.value;
+        obj["name"] = firstName.value + " " + secondName.value;
+        obj["password"] = newPassword.value;
+        obj["token"] = newToken.value;
+        var myJSON = JSON.stringify(obj);
+    
+        var xhttp = new XMLHttpRequest();
+        var url = "/user";
+        xhttp.open("POST", url, true);
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.send(myJSON);
+
+        if(true) {
+            alert("Account erfolgreich angelegt!");
+            window.location.href = "/#";
+        }
+        else
+        {
+            alert("Account anlegen fehlgeschlagen!");
+        }
+    }
+
 }
