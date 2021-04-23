320dd2351e8eed50f7c103090c6d8d903c88f978
JannikWell <jawell77@gmail.com>
2021-04-23 13:41:18 +0200

    Make use of API call to get the preview html
    Also adapted the design

--- Source/Backend/vereinswebseite/static/src/create_blog_post.js
+++ Source/Backend/vereinswebseite/static/src/create_blog_post.js
@@ -1,15 +1,36 @@
+var frontendHelper = new FrontendHelper();
 var isShowingPreview = false;
 function swapShowingPreview() {
     var markdown = document.getElementById("markdown");
+    var jsonObj = {};
+    jsonObj["content"] = markdown.value;
     if (!isShowingPreview && markdown.value != "") {
-        document.getElementById("markdown_preview").innerHTML =
-            marked(markdown.value);
+        /*document.getElementById("markdown_preview").innerHTML =
+        marked(markdown.value);
         document.getElementById("swapShowingPreview_button").innerHTML = "Vorschau ausblenden";
-        isShowingPreview = true;
+        document.getElementById("preview_div").style.display = "block";
+        isShowingPreview = true;*/
+        function myOnloadFunction(response) {
+            if (response == null) {
+                alert("Kommunikation mit Server fehlgeschlagen!");
+                return;
+            }
+            else if (response.success) {
+                document.getElementById("markdown_preview").innerHTML = response.html;
+                document.getElementById("swapShowingPreview_button").innerHTML = "Vorschau ausblenden";
+                document.getElementById("preview_div").style.display = "block";
+                isShowingPreview = true;
+            }
+            else {
+                alert("Anzeigen der Vorschau fehlgeschlagen!" + "\n➔ " + response.errors[0].title + ".");
+            }
+        }
+        frontendHelper.makeHttpRequest("POST", "/blog_posts/render_preview", jsonObj, myOnloadFunction);
     }
     else {
         document.getElementById("markdown_preview").innerHTML = "";
         document.getElementById("swapShowingPreview_button").innerHTML = "Vorschau anzeigen";
+        document.getElementById("preview_div").style.display = "none";
         isShowingPreview = false;
     }
 }
--- Source/Backend/vereinswebseite/static/src/typescript/create_blog_post.ts
+++ Source/Backend/vereinswebseite/static/src/typescript/create_blog_post.ts
@@ -1,18 +1,43 @@
+let frontendHelper = new FrontendHelper()
 var isShowingPreview: Boolean = false;
 
 function swapShowingPreview() {
     var markdown = document.getElementById("markdown") as HTMLInputElement;
+    var jsonObj = {};
+        jsonObj["content"] = markdown.value;
+
+
 
     if(!isShowingPreview && markdown.value != "") {
-        document.getElementById("markdown_preview").innerHTML =
+        /*document.getElementById("markdown_preview").innerHTML =
         marked(markdown.value);        
         document.getElementById("swapShowingPreview_button").innerHTML = "Vorschau ausblenden";
-        isShowingPreview = true;
+        document.getElementById("preview_div").style.display = "block";
+        isShowingPreview = true;*/
+        
+        function myOnloadFunction(response) {
+            if(response == null) {
+                alert("Kommunikation mit Server fehlgeschlagen!");
+                return
+            }
+            else if (response.success)
+            {
+                document.getElementById("markdown_preview").innerHTML = response.html;
+                document.getElementById("swapShowingPreview_button").innerHTML = "Vorschau ausblenden";
+                document.getElementById("preview_div").style.display = "block";
+                isShowingPreview = true;
+            }
+            else {
+                alert("Anzeigen der Vorschau fehlgeschlagen!" + "\n➔ " + response.errors[0].title + ".");
+            }
+    }
+    frontendHelper.makeHttpRequest("POST", "/blog_posts/render_preview", jsonObj, myOnloadFunction);
+
     }
     else{
         document.getElementById("markdown_preview").innerHTML = "";
-
-        document.getElementById("swapShowingPreview_button").innerHTML = "Vorschau anzeigen";
+        document.getElementById("swapShowingPreview_button").innerHTML = "Vorschau anzeigen";        
+        document.getElementById("preview_div").style.display = "none";
         isShowingPreview = false;
     }
 }
