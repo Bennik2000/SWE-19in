99209a5a56fa3f847031f81b0bf27d163f1735e4
Benedikt Sorge <b.sorge@t-online.de>
2021-04-28 17:02:22 +0200

    added markdown render on preview

--- Source/Backend/vereinswebseite/static/src/blog_overview.js
+++ Source/Backend/vereinswebseite/static/src/blog_overview.js
@@ -5,49 +5,56 @@ function get_all_blogs() {
         if (response) {
             if (response.success == true) {
                 response.blog_posts.forEach(function (post) {
-                    var col = document.createElement("div");
-                    col.classList.add("col-md-12");
-                    col.classList.add("mb-3");
-                    var container = document.createElement("div");
-                    container.classList.add("container");
-                    container.classList.add("position-relative");
-                    container.setAttribute("id", "post");
-                    var row = document.createElement("div");
-                    row.classList.add("row");
-                    var post_titel = document.createElement("h3");
-                    post_titel.innerHTML = post.title;
-                    post_titel.classList.add("col-sm-12");
-                    post_titel.setAttribute("id", "post_titel");
-                    var post_meta = document.createElement("div");
-                    post_meta.classList.add("col-sm-12");
-                    post_meta.classList.add("mb-1");
-                    post_meta.setAttribute("id", "post_meta");
-                    var post_author = document.createElement("span");
-                    post_author.setAttribute("id", "post_author");
-                    post_author.innerHTML = post.author;
-                    var post_content = document.createElement("div");
-                    post_content.classList.add("col-md-12");
-                    post_content.setAttribute("id", "post_content");
-                    if (post.content.length > 300) {
-                        post_content.innerHTML = ((String)(post.content)).substr(0, 300) + "...";
+                    function myOnloadFunction(response) {
+                        var col = document.createElement("div");
+                        col.classList.add("col-md-12");
+                        col.classList.add("mb-3");
+                        var container = document.createElement("div");
+                        container.classList.add("container");
+                        container.classList.add("position-relative");
+                        container.setAttribute("id", "post");
+                        var row = document.createElement("div");
+                        row.classList.add("row");
+                        var post_titel = document.createElement("h3");
+                        post_titel.innerHTML = post.title;
+                        post_titel.classList.add("col-sm-12");
+                        post_titel.setAttribute("id", "post_titel");
+                        var post_meta = document.createElement("div");
+                        post_meta.classList.add("col-sm-12");
+                        post_meta.classList.add("mb-1");
+                        post_meta.setAttribute("id", "post_meta");
+                        var post_author = document.createElement("span");
+                        post_author.setAttribute("id", "post_author");
+                        post_author.innerHTML = post.author;
+                        var post_separator = document.createElement("hr");
+                        var post_content = document.createElement("div");
+                        post_content.classList.add("col-md-12");
+                        post_content.setAttribute("id", "post_content");
+                        if (post.content.length > 300) {
+                            post_content.innerHTML = response.html;
+                        }
+                        else {
+                            post_content.innerHTML = response.html;
+                        }
+                        var post_more = document.createElement("div");
+                        post_more.classList.add("col-md-1");
+                        post_more.classList.add("btn");
+                        post_more.setAttribute("id", "post_more");
+                        post_more.setAttribute("href", "#");
+                        post_more.innerHTML = "mehr...";
+                        post_meta.appendChild(post_author);
+                        row.appendChild(post_titel);
+                        row.appendChild(post_meta);
+                        row.appendChild(post_separator);
+                        row.appendChild(post_content);
+                        row.appendChild(post_more);
+                        container.appendChild(row);
+                        col.appendChild(container);
+                        post_list.appendChild(col);
                     }
-                    else {
-                        post_content.innerHTML = post.content;
-                    }
-                    var post_more = document.createElement("div");
-                    post_more.classList.add("col-md-1");
-                    post_more.classList.add("btn");
-                    post_more.setAttribute("id", "post_more");
-                    post_more.setAttribute("href", "#");
-                    post_more.innerHTML = "mehr...";
-                    post_meta.appendChild(post_author);
-                    row.appendChild(post_titel);
-                    row.appendChild(post_meta);
-                    row.appendChild(post_content);
-                    row.appendChild(post_more);
-                    container.appendChild(row);
-                    col.appendChild(container);
-                    post_list.appendChild(col);
+                    var jsonObj = {};
+                    jsonObj["content"] = post.content;
+                    frontendHelper.makeHttpRequest("POST", "/blog_posts/render_preview", jsonObj, myOnloadFunction);
                 });
             }
             else {
--- Source/Backend/vereinswebseite/static/src/typescript/blog_overview.ts
+++ Source/Backend/vereinswebseite/static/src/typescript/blog_overview.ts
@@ -10,59 +10,70 @@ function get_all_blogs(){
             if(response.success==true){
                 
                 response.blog_posts.forEach(post => {
-                    
-                    let col = document.createElement("div");
-                    col.classList.add("col-md-12");
-                    col.classList.add("mb-3");
+                    function myOnloadFunction(response){
 
-                    let container = document.createElement("div");
-                    container.classList.add("container");
-                    container.classList.add("position-relative");
-                    container.setAttribute("id","post");
+                        let col = document.createElement("div");
+                        col.classList.add("col-md-12");
+                        col.classList.add("mb-3");
+    
+                        let container = document.createElement("div");
+                        container.classList.add("container");
+                        container.classList.add("position-relative");
+                        container.setAttribute("id","post");
+    
+                        let row = document.createElement("div");
+                        row.classList.add("row");
+    
+                        let post_titel = document.createElement("h3");
+                        post_titel.innerHTML=post.title;
+                        post_titel.classList.add("col-sm-12");
+                        post_titel.setAttribute("id","post_titel");
+    
+                        let post_meta = document.createElement("div");
+                        post_meta.classList.add("col-sm-12");
+                        post_meta.classList.add("mb-1");
+                        post_meta.setAttribute("id","post_meta");
+    
+                        let post_author = document.createElement("span");
+                        post_author.setAttribute("id","post_author");
+                        post_author.innerHTML=post.author;
+                        
+                        let post_separator = document.createElement("hr");
 
-                    let row = document.createElement("div");
-                    row.classList.add("row");
-
-                    let post_titel = document.createElement("h3");
-                    post_titel.innerHTML=post.title;
-                    post_titel.classList.add("col-sm-12");
-                    post_titel.setAttribute("id","post_titel");
-
-                    let post_meta = document.createElement("div");
-                    post_meta.classList.add("col-sm-12");
-                    post_meta.classList.add("mb-1");
-                    post_meta.setAttribute("id","post_meta");
-
-                    let post_author = document.createElement("span");
-                    post_author.setAttribute("id","post_author");
-                    post_author.innerHTML=post.author;
-
-                    let post_content = document.createElement("div");
-                    post_content.classList.add("col-md-12");
-                    post_content.setAttribute("id","post_content");
-                    if(post.content.length>300){
-                        post_content.innerHTML=((String)(post.content)).substr(0,300) +"...";
+                        let post_content = document.createElement("div");
+                        post_content.classList.add("col-md-12");
+                        post_content.setAttribute("id","post_content");
+                        if(post.content.length>300){
+                            post_content.innerHTML=response.html;
+                        }
+                        else{
+                            post_content.innerHTML=response.html;
+                        }
+                        
+                        let post_more = document.createElement("div");
+                        post_more.classList.add("col-md-1");
+                        post_more.classList.add("btn");
+                        post_more.setAttribute("id","post_more");
+                        post_more.setAttribute("href","#");
+                        post_more.innerHTML = "mehr...";
+                        post_meta.appendChild(post_author);
+    
+                        row.appendChild(post_titel);
+                        row.appendChild(post_meta);
+                        row.appendChild(post_separator);
+                        row.appendChild(post_content);
+                        row.appendChild(post_more);
+                        container.appendChild(row);
+                        col.appendChild(container);
+    
+                        post_list.appendChild(col);
                     }
-                    else{
-                        post_content.innerHTML=post.content;
-                    }
+                    let jsonObj ={};
+                    jsonObj["content"]=post.content
                     
-                    let post_more = document.createElement("div");
-                    post_more.classList.add("col-md-1");
-                    post_more.classList.add("btn");
-                    post_more.setAttribute("id","post_more");
-                    post_more.setAttribute("href","#")
-                    post_more.innerHTML = "mehr...";
-                    post_meta.appendChild(post_author);
-
-                    row.appendChild(post_titel);
-                    row.appendChild(post_meta);
-                    row.appendChild(post_content);
-                    row.appendChild(post_more);
-                    container.appendChild(row);
-                    col.appendChild(container);
-
-                    post_list.appendChild(col);
+                    frontendHelper.makeHttpRequest("POST", "/blog_posts/render_preview", jsonObj, myOnloadFunction);
+                    
+                   
 
 
                 });
