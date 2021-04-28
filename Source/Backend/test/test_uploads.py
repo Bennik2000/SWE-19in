2ff16bd2b4838583a371d7e4b07a6807d4a8ae03
CodingMarco <codingmarco@gmail.com>
2021-04-28 15:49:50 +0200

    Added unit test to test the serving of uploaded files

--- Source/Backend/test/test_uploads.py
+++ Source/Backend/test/test_uploads.py
@@ -3,13 +3,14 @@ import unittest
 import filecmp
 
 import vereinswebseite
-from flask import Response
 import flask_uploads
+from http import HTTPStatus
+from flask import Response
 from werkzeug.datastructures import FileStorage
 from test.test_utils import setup_test_app, create_and_login_test_user
 
 
-class BlogPostTest(unittest.TestCase):
+class UploadsTest(unittest.TestCase):
     # The root_path is Backend/test when a test is run. So we have to adjust it here.
     UPLOADS_DIRECTORY = os.path.join(vereinswebseite.app.root_path, "..", "uploads")
     TEST_FILE_PATH = "upload_test.jpeg"
@@ -20,7 +21,7 @@ class BlogPostTest(unittest.TestCase):
         flask_uploads.configure_uploads(vereinswebseite.app, vereinswebseite.images)
         self.app = setup_test_app()
 
-    def test_given_logged_in_then_success_true(self):
+    def test_given_logged_in_then_correct_return_value(self):
         create_and_login_test_user(self.app)
         response = self._send_upload_request()
         print(f"JSON response: {response.json}")
@@ -28,6 +29,7 @@ class BlogPostTest(unittest.TestCase):
         success = response.json["success"]
         self.assertIsNotNone(success)
         self.assertTrue(success)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
         os.remove(os.path.join(self.UPLOADS_DIRECTORY, response.json["filename"]))
 
     def test_given_logged_in_then_uploaded_correctly(self):
@@ -41,6 +43,17 @@ class BlogPostTest(unittest.TestCase):
         files_equal = filecmp.cmp(self.TEST_FILE_PATH, uploaded_file_path)
         self.assertTrue(files_equal, "The uploaded file isn't the same as the original file")
         os.remove(uploaded_file_path)
+
+    def test_given_file_uploaded_then_served_correctly(self):
+        create_and_login_test_user(self.app)
+        upload_response = self._send_upload_request()
+        filename = upload_response.json["filename"]
+
+        download_response = self.app.get(f"/_uploads/images/{filename}")
+        self.assertEqual(download_response.status_code, HTTPStatus.OK)
+        # flask's helpers.send_file() doesn't close files automatically, so we have to do that manually:
+        download_response.close()
+        os.remove(os.path.join(self.UPLOADS_DIRECTORY, filename))
 
     def test_given_not_logged_in_then_not_uploaded(self):
         response = self._send_upload_request()
