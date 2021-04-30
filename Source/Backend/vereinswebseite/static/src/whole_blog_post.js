2506eb2ce9297e603a9f3acd3370180d3dbc66a8
JannikWell <jawell77@gmail.com>
2021-04-30 12:01:47 +0200

    Implemented the button click events

--- Source/Backend/vereinswebseite/static/src/whole_blog_post.js
+++ Source/Backend/vereinswebseite/static/src/whole_blog_post.js
@@ -1,15 +1,23 @@
 var frontendHelper = new FrontendHelper();
-function get_post_info() {
+function editBlogPost() {
+    var id = document.getElementById("id").innerHTML.toString();
+    window.location.href = "/blog_posts/edit?posts_id=" + id;
 }
-function post_edit() {
-}
-function post_save() {
-}
-function post_delete() {
-}
-function new_post() {
-}
-function comment_edit() {
-}
-function comment_save() {
+function deleteBlogPost() {
+    var jsonObj = {};
+    jsonObj["id"] = document.getElementById("id").innerHTML;
+    function myOnloadFunction(response) {
+        if (response == null) {
+            alert("Kommunikation mit Server fehlgeschlagen!");
+            return;
+        }
+        else if (response.success) {
+            alert("Artikel erfolgreich gelöscht!");
+            window.location.href = "/#"; //TODO: Link to the blog post overview of all post
+        }
+        else {
+            alert("Löschen fehlgeschlagen!" + "\n➔ " + response.errors[0].title + ".");
+        }
+    }
+    frontendHelper.makeHttpRequest("DELETE", "/blog_posts/delete", jsonObj, myOnloadFunction);
 }
