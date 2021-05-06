68265680c190e994362c63e1f4dfb69c4b023568
JonasHille <71066066+JonasHille@users.noreply.github.com>
2021-05-06 09:41:15 +0200

    Adds a button to delete a post, which can be seen only with the appropriate permissions

--- Source/Backend/vereinswebseite/static/src/typescript/create_and_edit_blog_post.ts
+++ Source/Backend/vereinswebseite/static/src/typescript/create_and_edit_blog_post.ts
@@ -281,3 +281,29 @@ function embedImageIntoMarkdown(filename) {
     var imageURL = "![](/_uploads/images/" + filename + ")";
     markdown.value += "  \n" + imageURL;
 }
+
+function deleteBlogPost() {
+
+    let wantsToCancel = confirm("Soll der Beitrag wirklich gelöscht werden?");
+    if (wantsToCancel) 
+    {
+        var jsonObj = {};
+        jsonObj["id"] = document.getElementById("id").innerHTML;
+
+        function myOnloadFunction(response) {
+            if(response == null)
+            {
+                alert("Kommunikation mit Server fehlgeschlagen!");
+                return;
+            }
+            else if (response.success) {
+                alert("Artikel erfolgreich gelöscht!");
+                window.location.href = "/#"; //TODO: Link to the blog post overview of all post
+            }
+            else {
+                alert("Löschen fehlgeschlagen!" + "\n➔ " + response.errors[0].title + ".");
+            }
+        }
+        frontendHelper.makeHttpRequest("DELETE", "/api/blog_posts/delete", jsonObj, myOnloadFunction); 
+    }
+}
