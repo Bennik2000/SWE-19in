873381524031283c87d0cfa56c74d82647a617b7
Bennik2000 <bennik.ko@gmail.com>
2021-05-12 17:35:57 +0200

    Implement cleanup of images when a blog post was deleted or edited

---
+++ Source/Backend/test/test_blog_post_delete_image_on_delete.py
@@ -0,0 +1,74 @@
+import os
+import pathlib
+
+from flask import Response
+from werkzeug.datastructures import FileStorage
+
+from test.base_test_case import BaseTestCase
+
+
+class DeleteBlogPostImagesTest(BaseTestCase):
+    UPLOADS_DIRECTORY = os.path.join(pathlib.Path(__file__).parent.absolute(), "..", "uploads")
+    TEST_FILE_PATH = os.path.join(pathlib.Path(__file__).parent.absolute(), "upload_test.jpeg")
+
+    def setUp(self) -> None:
+        super().setUp(custom_config={
+            "UPLOADED_IMAGES_DEST": self.UPLOADS_DIRECTORY
+        })
+
+    def test_on_delete_post_images_are_deleted(self):
+        self.create_and_login_test_user()
+        uploaded_file_path = self.create_blog_post_with_image()
+
+        self.delete_blog_post()
+
+        uploaded_file_exists = os.path.isfile(uploaded_file_path)
+        self.assertFalse(uploaded_file_exists, "The uploaded file was not deleted")
+
+    def test_on_update_post_images_are_deleted(self):
+        self.create_and_login_test_user()
+        uploaded_file_path = self.create_blog_post_with_image()
+
+        self.edit_blog_post("# New content with no image")
+
+        uploaded_file_exists = os.path.isfile(uploaded_file_path)
+        self.assertFalse(uploaded_file_exists, "The uploaded file was not deleted")
+
+    def create_blog_post_with_image(self):
+        response = self._send_upload_request()
+        image_filename = response.json["filename"]
+        self.create_blog_post("#Title\n![Alt-text](" + image_filename + "){: style='width: 5vw;'}")
+        uploaded_file_path = os.path.join(self.UPLOADS_DIRECTORY, image_filename)
+        uploaded_file_exists = os.path.isfile(uploaded_file_path)
+        self.assertTrue(uploaded_file_exists, "The uploaded file doesn't exist in the upload directory")
+        return uploaded_file_path
+
+    def delete_blog_post(self):
+        self.app.delete("/api/blog_posts/delete", json={
+            "id": "1",
+        })
+
+    def create_blog_post(self, content):
+        self.app.post("/api/blog_posts", json={
+            "title": "Title",
+            "content": content,
+        })
+
+    def edit_blog_post(self, new_content):
+        self.app.put("/api/blog_posts/update", json={
+            "id": "1",
+            "title": "Title",
+            "content": new_content,
+        })
+
+    def _send_upload_request(self) -> Response:
+        test_file = FileStorage(stream=open(self.TEST_FILE_PATH, "rb"))
+
+        response = self.app.post(
+            "/api/upload_image",
+            data={"image": test_file},
+            content_type='multipart/form-data'
+        )
+
+        return response
+
