2d69ab014893d2376892013dc4ff9ddb7fc93c13
Bennik2000 <bennik.ko@gmail.com>
2021-05-16 21:45:49 +0200

    Use has_roles from user class directly

--- Source/Backend/vereinswebseite/permissions.py
+++ Source/Backend/vereinswebseite/permissions.py
@@ -4,15 +4,14 @@ from vereinswebseite.models.blog_post import BlogPost
 def can_user_edit_blog_post(user, post: BlogPost) -> bool:
     if not user.is_authenticated:
         return False
-    return has_role(user, "Webmaster") or post.author_id == user.id
+    return _is_webmaster(user) or post.author_id == user.id
 
 
 def can_user_delete_blog_post(user, post: BlogPost) -> bool:
     if not user.is_authenticated:
         return False
-    return has_role(user, "Webmaster") or post.author_id == user.id
+    return _is_webmaster(user) or post.author_id == user.id
 
 
-def has_role(user, role: str) -> bool:
-    roles = [role.name for role in user.roles]
-    return role in roles
+def _is_webmaster(user) -> bool:
+    return user.has_roles("Webmaster")
